d833abf37879e441e3ebc44c823cc93b33fb3324
Hamzah <humza.murtaza@gmail.com>
2017-10-04 18:14:42 +0500

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,17 @@
+<?php
+session_start();
+
+include 'dbh.php';
+
+$first = $_POST['first'];
+$last = $_POST['last'];
+$uid = $_POST['uid'];
+$pwd = $_POST['pwd'];
+
+$sql = "INSERT INTO person (first, last, uid, pwd) 
+VALUES ('$first','$last','$uid','$pwd')";
+$result = mysqli_query($conn,$sql);
+
+header("Location :index.php");
+
+?>
